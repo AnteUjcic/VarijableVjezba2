12e5f7ef00f2183f213eacdc420d0667e95eead2
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2020-11-09 08:46:55 +0100

    Add files via upload

---
+++ VarijableVjezba2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarijableVjezba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int brojSto = 100;
+            int brojMinusTisucu = -1000;
+            char slovoA = 'a';
+            float decimalniBroj3Decimale =3.123f;
+            double decimalniBroj9Decimale = 3.123456789;
+            bool boleanTrue= true;
+            string stringVarijablaUKojojJeTrue= "\"true\"";
+            
+
+
+        
+        }
+    }
+}
